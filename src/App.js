3dbdebb1b5f06ce2684427ecdf1f5d6d0bcd31ae
DukeSNS <116050207+DukeSNS@users.noreply.github.com>
2024-03-15 00:02:00 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import LoginForm from './LoginForm';
+
+
+function App() {
+    return (
+        <div className="App">
+            <LoginForm />
+        </div>
+    );
+}
+
+export default App;
